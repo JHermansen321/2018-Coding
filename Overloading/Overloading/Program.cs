c16f20006b42485ec738fcedcc133bd3e821e1b9
JHermansen321 <144226@student.asd20.org>
2018-11-08 13:09:51 -0700

    method overloading

---
+++ Overloading/Overloading/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int[] array = { 1, 7, 6, 9, 0 };
+            int[,] ARRAY = { { 1, 2, 6, 5, 7 }, { 2, 3, 5, 4, 5 } };
+            int[][] DIF = { new int[] { 5, 8, 3, 5 }, new int[] { 2, 5 }, new int[] { 7, 4, 6 } };
+            Print(a);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Print(array);
+            Console.WriteLine();
+
+            Print(ARRAY);
+            Console.WriteLine();
+
+            Print(DIF);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+
+        public static void Print(int a)
+        {
+            Console.WriteLine("Int" + a);
+        }
+
+        public static void Print(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("|" + array[i] + "|");
+            }
+        }
+
+        public static void Print(int[,] ARRAY)
+        {
+            for (int i = 0; i < ARRAY.GetLength(0); i++)
+            {
+                for (int j = 0; j < ARRAY.GetLength(1); j++)
+                {
+                    Console.Write("|" + ARRAY[i, j] + "|");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void Print(int[][] DIF)
+        {
+            for (int i = 0; i < DIF.Length; i++)
+            {
+                for (int j = 0; j < DIF[i].Length; j++)
+                {
+                    Console.Write("|" + DIF[i][j] + "|");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
