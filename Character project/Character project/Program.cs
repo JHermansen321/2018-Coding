86a5399d747e1a71fda227b1e3e78e28519b6a52
JHermansen321 <144226@student.asd20.org>
2018-12-21 09:18:44 -0700

    Final Commit

---
+++ Character project/Character project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_project
+{
+    class character
+    {
+        public string name;
+        public string eyecolor;
+        public string haircolor;
+        public string skincolor;
+        public int age;
+        public character(string name, string eyecolor, string haircolor, string skincolor, int age)
+        {
+            this.name = name;
+            this.eyecolor = eyecolor;
+            this.haircolor = haircolor;
+            this.skincolor = skincolor;
+            this.age = age; 
+
+
+        }
+
+    }
+}
---
+++ Character project/Character project/character.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_project
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int menuanswer = 0;
+            string nameanswer = "";
+            string eyeanswer = "";
+            string hairanswer = "";
+            string skinanswer = "";
+
+            do
+            {
+                Console.WriteLine"******************";
+                Console.WriteLine"1. create a character.";
+                Console.WriteLine"2. modify a character.";
+                Console.WriteLine"3. delete a character.";
+                Console.WriteLine"4. load a character";
+                Console.WriteLine"5. list a character";
+                Console.WriteLine"6. exit program";
+                Console.WriteLine"******************";
+                menuanswer = int.Parse(Console.ReadLine());
+
+                switch (menuanswer)
+                {
+                    case 1: 
+                        Chracter rtnCharacter
+                }
+            }
+        }
+    }
+}
